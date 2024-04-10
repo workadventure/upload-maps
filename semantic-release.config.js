8a7548ec7fc4d0ebdc1cd326bed34f6d0bca3c80
alexandre artus <artusalexandre@yahoo.fr>
2024-04-10 16:53:56 +0200

    ajout du workflow github action update node version

--- semantic-release.config.js
+++ semantic-release.config.js
@@ -3,6 +3,5 @@ export const plugins = [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
-    "@semantic-release/git",
-    "@semantic-release/github"
+    "@semantic-release/git"
 ];
