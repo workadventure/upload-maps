8cc31954eddd1d8ff0b8bb0925554fda410b6725
alexandre artus <artusalexandre@yahoo.fr>
2024-04-11 11:33:02 +0200

    fix(core): add compiler for typescript

--- semantic-release.config.js
+++ semantic-release.config.js
@@ -3,5 +3,6 @@ export const plugins = [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
-    "@semantic-release/git"
+    "@semantic-release/git",
+    "@semantic-release/github"
 ];
