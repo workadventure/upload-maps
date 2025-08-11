0254a73109790c60210868e1838d75a52d9cb21d
Jade-Keina POGNON <jade.keina@gmail.com>
2025-08-11 17:01:04 +0200

    Fixed the CI

--- src/upload.ts
+++ src/upload.ts
@@ -3,6 +3,9 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */ 
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+
 
 import * as fs from "fs";
 import archiver from "archiver";
